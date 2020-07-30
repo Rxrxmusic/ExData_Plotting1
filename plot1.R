8eb4ee067e0c0727443cd8ca67a69b9608a038c0
Rxrxmusic <rogermaxia@yahoo.com>
2020-07-30 10:55:56 -0700

    plot1

---
+++ plot1.R
@@ -0,0 +1,10 @@
+setwd('C:/Users/Roger Xia/Desktop/Coursera Exploratory Data Analysis')
+
+data <- read.csv('household_power_consumption.txt', header=T, sep=';', na.strings='?',
+                 nrows=2075259, check.names=F, stringsAsFactors=F, comment.char='', quote='\"')
+
+subdata <- subset(data, Date %in% c('1/2/2007', '2/2/2007'))
+subdata$Date <- as.Date(subdata$Date, format='%d/%m/%Y')
+
+hist(subdata$Global_active_power, main='Global Active Power',
+     xlab='Global Active Power (kilowatts)', ylab='Frequency', col='Red')
