6b97c9a8cfe14c34a991831b93aa7fd073b1312f
Rxrxmusic <rogermaxia@yahoo.com>
2020-07-30 10:58:20 -0700

    updated

--- plot1.R
+++ plot1.R
@@ -8,3 +8,6 @@ subdata$Date <- as.Date(subdata$Date, format='%d/%m/%Y')
 
 hist(subdata$Global_active_power, main='Global Active Power',
      xlab='Global Active Power (kilowatts)', ylab='Frequency', col='Red')
+
+png('plot1.png', width=480, height=480)
+dev.off()
